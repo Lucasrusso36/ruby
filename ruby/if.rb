08894df9c556be665a8273d16325a63793f05ea3
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-05-17 23:15:28 -0300

    pasta ruby / html

--- ruby/if.rb
+++ ruby/if.rb
@@ -1,3 +1,6 @@
+#Expressão que verifica se uma condição é verdadeira(true), e a partir deste resultado determina 
+#se as instruções dentro de seu corpo serão ou não executadas.
+
 day = 'Sunday' 
 lunch = 'normal'
 
