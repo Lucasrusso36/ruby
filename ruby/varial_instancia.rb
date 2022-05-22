0df0d2c099ad2ce68e1c8048e614e4bc81d9c76f
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-05-21 22:24:31 -0300

    varial / construtor / atributos / block

---
+++ ruby/varial_instancia.rb
@@ -0,0 +1,19 @@
+class User
+  def add(name)
+    @name = name
+    puts "User adicionado"
+    hello
+  end
+
+  def hello
+    puts "Seja bem vindo, #{@name}!"
+  end
+end
+
+user = User.new
+user.add('João')
+
+
+#Seu nome começa com o símbolo @.
+
+#Semelhante a variável de classe, tendo como única diferença o valor que não é compartilhado entre todas as instâncias de sua classe.
---
+++ ruby/varial_local.rb
@@ -0,0 +1,21 @@
+class Bar
+  def foo
+    # Pode ser definida como local ou _local
+    local = 'local é acessada apenas dentro deste metodo'
+    puts local
+  end
+
+  def example
+    local
+  end
+end
+
+bar = Bar.new
+bar.foo
+
+
+#É declarada com a primeira letra de seu nome sendo uma letra minúscula ou sublinhado.
+
+#Pode ser acessada apenas onde foi criada. Por exemplo, se você definir uma variável local dentro de 
+#de uma classe ela estará disponível apenas dentro desta classe, se a definiu dentro de um método conseguirá
+#acessá-la apenas dentro deste método e assim por diante.
